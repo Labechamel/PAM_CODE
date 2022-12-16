7b1c134c521df4709e644d493c25f1904e409b61
Labechamel <bechusimon@hotmail.fr>
2022-12-16 12:00:36 +0100

    **************************************************

--- Start.py
+++ Start.py
@@ -5,26 +5,30 @@ from qt.IHMMachines import *
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QTableWidgetItem, QMainWindow
 
-# create a function that ask in the terminal if you want drop the table pam and del the csv file
 
 def run():
     print("****************************************************************************************")
-    print ("start")
+    print("                                      start                                             ")
+    print("****************************************************************************************")
     print(' ')
 
     mydb, mycursor = connect_to_mysql()
 
     #drop_table()
-    print('we are connected to the database')
+    print (' ')
+    print("----------------------------------------------------------------------------------------")
+    print('                      we are connected to the database                                  ')
+    print("----------------------------------------------------------------------------------------")
     print(' ')
 
-    print('we send data to the database')
+    print('                 ---------> we send data to the database                                 ')
     print('....')
     time.sleep(1)
     print('....')
     time.sleep(1)
     print('....')
     time.sleep(1)
+    print (' ')
 
     create_table(mydb, mycursor)
     init_csv()
@@ -32,18 +36,27 @@ def run():
     for i in range(1):
         generate_csv()    
     send_to_mysql(mydb, mycursor)
-    print('data sent to the database')
+    print("----------------------------------------------------------------------------------------")
+    print('                         data sent to the database                                      ')
+    print("----------------------------------------------------------------------------------------")
     print(' ')
 
-    print('we collect data from the database')
+    print('         ------------> we collect data from the database                                ')
     print('....')
     time.sleep(1)
     print('....')
     time.sleep(1)
-    print('we have collected data from the database')
-    print(' ')
+    print (' ')
 
-    print('we display the data')
+    
+    print("----------------------------------------------------------------------------------------")
+    print('                  we have collected data from the database'                              )
+    print("----------------------------------------------------------------------------------------")
+    print(' ')
+    
+    print("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+    print(' ')
+    print('                           we display the data                                          ')
     print('....')
     time.sleep(1)
     print('....')
@@ -53,25 +66,41 @@ def run():
 
 
 
-
+# create a function that ask in the terminal if you want drop the table pam and del the csv file
 def main_nous():
-    drop = input("Do you want to drop the table pam and the file generation_csv_data? (y/n)")
+    drop = input("Do you want to drop the table pam and the file generation_csv_data? (y/n) : ")
     if drop == "y":
         mydb, mycursor = connect_to_mysql()
         mycursor.execute("DROP TABLE pam")
         mydb.commit()
-        print("table pam droped")
+        print(' ')
+        print("                                   table pam droped                                     ")
+        print(' ')
         os.remove("generate_csv/generation_csv_data.csv")
-        print("file generate_csv_data.csv deleted")
+        time.sleep(1)
+        print("                          file generate_csv_data.csv deleted                            ")
+        print(' ')
         print("****************************************************************************************")
+        print(' ')
+        
+        time.sleep(1)
         drop2 = input("Do you want to create a new csv file (y/n)")
+        print(' ')
+        
         if drop2 == "y":
             init_csv()
-            print("new csv file created")
-        print("****************************************************************************************")
-        print("Now we run the program")
-        print("****************************************************************************************")
-        run()        
+            time.sleep(1)
+            print("                             new csv file created                                       ")
+            print("****************************************************************************************")
+            print(' ')
+            time.sleep(1)
+            print("                          Now we run the program                                        ")
+            print("****************************************************************************************")
+            print (' ')
+            print (' ')
+            time.sleep(2)
+
+            run()        
     else:
         run()
 
--- run.bat
+++ run.bat
@@ -1,3 +1,16 @@
-title Application PAM ExcelCar V1.0
+title Application PAM ExcelCAr V1.0
+
+ECHO OFF
+ECHO ~                                                                                        ~
+ECHO ~                                                                                        ~
+ECHO *******************************************************************************************
+ECHO *********                 Bienvenue sur l application Excelcar                   **********
+ECHO *******************************************************************************************
+ECHO ~                                                                                         ~                                                                                            
+ECHO ~                                                                                         ~                                                                                            
+ECHO *********            appuyer sur entrer pour lancer l application                **********
+ECHO ~                                                                                         ~                                                                                            
+ECHO ~                                                                                         ~
+pause
 python Start.py
 pause
